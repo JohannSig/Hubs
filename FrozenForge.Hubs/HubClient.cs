b9a08c6635ddc1c0d4dd8ca8ce70f1badb023240
Jóhann Sigurðsson <517721+JohannSig@users.noreply.github.com>
2022-02-19 13:21:21 +0000

    Amended commit

---
+++ FrozenForge.Hubs/HubClient.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FrozenForge.Hubs
+{
+
+    public abstract class HubClient<THubClientMethods> : IHubClient<THubClientMethods>
+	{
+		protected HubClient(Uri hubUri)
+			: this(hubUri, cookie: default)
+        {
+        }
+
+		protected HubClient(
+			Uri hubUri, 
+			HttpContext httpContext, 
+			string cookieName)
+			: this(hubUri, new Cookie
+			{
+				Name = cookieName,
+				Value = httpContext.Request.Cookies.TryGetValue(cookieName, out var cookie) ? cookie : default,
+				Domain = httpContext.Request.Host.Host
+			})
+        {
+        }
+
+		private HubClient(Uri hubUri, Cookie cookie)
+		{
+			HubConnection = new HubConnectionBuilder()
+				.AddJsonProtocol(config => config.PayloadSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve)
+				.WithUrl(hubUri,
+                config =>
+                {
+                    config.Cookies.Add(cookie);
+                })
+				.Build();
+		}
+
+		public HubConnection HubConnection { get; }
+
+		public IDisposable On<TResult>(Func<TResult, Task> handler)
+			=> HubConnection.On(handler.Method.Name, handler);
+
+        public Task ConnectAsync() => HubConnection.StartAsync();
+
+        public ValueTask DisposeAsync()
+			=> HubConnection.DisposeAsync();
+	}
+}
---
+++ FrozenForge.Hubs/Interfaces/IHubClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FrozenForge.Hubs
+{
+    public interface IHubClient<THubClientMethods> : IAsyncDisposable
+	{
+		Task ConnectAsync();
+		
+		IDisposable On<TResult>(Func<TResult, Task> handler);
+	}
+}
