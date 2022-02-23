e3f0727c22e44e61a174fffaa798f33c42911dd9
Jóhann Sigurðsson <517721+JohannSig@users.noreply.github.com>
2022-02-23 00:18:57 +0000

    Added support for invoking by referencing HubMethod name in a func

--- FrozenForge.Hubs/HubClient.cs
+++ FrozenForge.Hubs/HubClient.cs
@@ -5,23 +5,25 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Net;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FrozenForge.Hubs
 {
-    public abstract class HubClient : IHubClient, IAsyncDisposable
-	{
+	public abstract class HubClient : IHubClient
+    {
 		protected HubClient(
 			ILogger logger,
 			Uri hubUri)
 			: this(logger, hubUri, cookie: default)
-        {
-        }
+		{
+		}
 
 		protected HubClient(
 			ILogger logger,
-			Uri hubUri, 
-			HttpContext httpContext, 
+			Uri hubUri,
+			HttpContext httpContext,
 			string cookieName)
 			: this(logger, hubUri, new Cookie
 			{
@@ -29,16 +31,16 @@ namespace FrozenForge.Hubs
 				Value = httpContext.Request.Cookies.TryGetValue(cookieName, out var cookie) ? cookie : default,
 				Domain = httpContext.Request.Host.Host
 			})
-        {
-        }
+		{
+		}
 
 		private HubClient(
 			ILogger logger,
-			Uri hubUri, 
+			Uri hubUri,
 			Cookie cookie)
 		{
 			Id = Guid.NewGuid();
-            this.Logger = logger;
+			this.Logger = logger;
 			HubConnection = new HubConnectionBuilder()
 				.AddJsonProtocol(config => config.PayloadSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve)
 				.WithUrl(
@@ -55,24 +57,25 @@ namespace FrozenForge.Hubs
 		}
 
 		public Guid Id { get; }
-		
-		public ILogger Logger { get; }        
-        
-		public HubConnection HubConnection { get; private set; }
-        
 
-        public IDisposable On<TResult>(Func<TResult, Task> handler)
+		public ILogger Logger { get; }
+
+		public HubConnection HubConnection { get; private set; }
+
+		public string ConnectionId => HubConnection.ConnectionId;
+
+		public IDisposable On<TResult>(Func<TResult, Task> handler)
 			=> HubConnection.On(handler.Method.Name, handler);
 
-        public async Task ConnectAsync()
-        {
-            await HubConnection.StartAsync();
+		public async Task ConnectAsync()
+		{
+			await HubConnection.StartAsync();
 
 			this.Logger.LogDebug($"HubClient {Id}: {HubConnection.State}");
-        }
+		}
 
-        public async ValueTask DisposeAsync()
-        {
+		public async ValueTask DisposeAsync()
+		{
 			if (HubConnection != null)
 			{
 				await HubConnection.DisposeAsync();
@@ -81,6 +84,43 @@ namespace FrozenForge.Hubs
 
 				this.Logger.LogDebug($"HubClient {Id}: Disposed");
 			}
+		}
+
+	}
+
+	public abstract class HubClient<THubMethods> : HubClient
+	{
+		protected HubClient(
+			ILogger logger,
+			Uri hubUri)
+			: base(logger, hubUri)
+		{
+		}
+
+		protected HubClient(
+			ILogger logger,
+			Uri hubUri,
+			HttpContext httpContext,
+			string cookieName)
+			: base(logger, hubUri, httpContext, cookieName)
+		{
+		}
+
+		public Task<TResult> InvokeAsync<TResult>(Func<THubMethods, Func<Task<TResult>>> methodFunc)
+		{
+			return this.HubConnection.InvokeAsync<TResult>(GetMethodName(methodFunc));
+		}
+
+		public async Task<TResult> InvokeAsync<TParameters, TResult>(Func<THubMethods, Func<TParameters, Task<TResult>>> methodFunc, TParameters parameters)
+		{
+			var methodName = GetMethodName(methodFunc);
+
+			return await this.HubConnection.InvokeAsync<TResult>(methodName, parameters);
+		}
+
+		private static string GetMethodName<TIn,TResult>(Func<TIn, TResult> methodFunc)
+        {
+			return Regex.Match(methodFunc.Method.Name, "<(.*?)>").Value.Trim('<', '>');
 		}
 	}
 }
--- FrozenForge.Hubs/Interfaces/IHubClient.cs
+++ FrozenForge.Hubs/Interfaces/IHubClient.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FrozenForge.Hubs
 {
-    public interface IHubClient : IAsyncDisposable
-	{
+	public interface IHubClient : IAsyncDisposable
+    {
 		Task ConnectAsync();
-		
+
+		string ConnectionId { get; }
+
 		IDisposable On<TResult>(Func<TResult, Task> handler);
+	}
+
+    public interface IHubClient<THubMethods> : IHubClient
+	{
+		Task<TResult> InvokeAsync<TResult>(Func<THubMethods, Func<Task<TResult>>> methodFunc);
 	}
 }
